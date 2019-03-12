932df79ef65a0a6d35f337dc5bc5c68d0169eab1
Brandon Pittman <gitlab@brandonpittman.net>
2019-03-12 09:12:15 +0900

    Add proof that this plugin works but that nobody understands how to set up Tailwind on their own at all

---
+++ ffs/gridsome.config.js
@@ -0,0 +1,14 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here requires a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'Gridsome',
+  plugins: [
+    {
+      use: 'gridsome-plugin-tailwindcss'
+    }
+  ]
+}
---
+++ ffs/src/layouts/Default.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1 class="text-red">hello world</h1>
+</template>
+
+<static-query>
+query {
+  metaData {
+    siteName
+  }
+}
+</static-query>
+
+<style>
+@tailwind preflight;
+
+@tailwind components;
+
+@tailwind utilities;
+</style>
