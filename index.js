369743474bed1c3d3ba6910d4b8754c1715f505f
Brandon Pittman <gitlab@brandonpittman.net>
2019-05-13 15:40:48 +0900

    Handle no config error

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
-const tailwindcss = require("tailwindcss");
+const tailwindcss = require('tailwindcss')
+const fs = require('fs')
+const { execSync } = require('child_process')
 
 class TailwindPlugin {
   static defaultOptions() {
     return {
-      config: "./tailwind.config.js"
-    };
+      config: './tailwind.config.js',
+    }
   }
 
   constructor(api, options) {
-    const tailwindOptions = options;
+    const tailwindOptions = options
+    if (!fs.existsSync(tailwindOptions)) {
+      console.error("Tailwind config file doesn't exist. Creating file now.")
+      execSync(`./node_modules/.bin/tailwind init ${tailwindOptions.config}`)
+    }
 
     api.chainWebpack(config => {
       config.module
-        .rule("css")
-        .oneOf("normal")
-        .use("postcss-loader")
+        .rule('css')
+        .oneOf('normal')
+        .use('postcss-loader')
         .tap(options => {
-          options.plugins.unshift(tailwindcss(tailwindOptions.config));
-          return options;
-        });
-    });
+          options.plugins.unshift(tailwindcss(tailwindOptions.config))
+          return options
+        })
+    })
   }
 }
 
-module.exports = TailwindPlugin;
+module.exports = TailwindPlugin
+
