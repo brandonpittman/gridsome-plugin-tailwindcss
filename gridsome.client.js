e72ba74775fcc3f9badab6a0caa7d5e705ea8075
Brandon Pittman <brandon@brandonpittman.net>
2019-12-25 12:44:02 +0900

    Update deps

--- gridsome.client.js
+++ gridsome.client.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-unassigned-import
 import 'tailwindcss/tailwind.css';
 
 export default function () {}
--- gridsome.server.js
+++ gridsome.server.js
@@ -1,84 +1,84 @@
 function TailwindPlugin(api, options) {
-  const {
-    tailwindConfig,
-    purgeConfig,
-    presetEnvConfig,
-    shouldImport,
-    shouldPurge,
-    shouldTimeTravel
-  } = options;
+	const {
+		tailwindConfig,
+		purgeConfig,
+		presetEnvConfig,
+		shouldImport,
+		shouldPurge,
+		shouldTimeTravel
+	} = options;
 
-  const postcssImport = require("postcss-import")();
+	const postcssImport = require('postcss-import')();
 
-  const tailwind = tailwindConfig
-    ? require("tailwindcss")(tailwindConfig)
-    : require("tailwindcss");
+	const tailwind = tailwindConfig ?
+		require('tailwindcss')(tailwindConfig) :
+		require('tailwindcss');
 
-  const postcssPresetEnv = require("postcss-preset-env")(presetEnvConfig);
+	const postcssPresetEnv = require('postcss-preset-env')(presetEnvConfig);
 
-  const purgecss = require("@fullhuman/postcss-purgecss")(purgeConfig);
+	const purgecss = require('@fullhuman/postcss-purgecss')(purgeConfig);
 
-  api.chainWebpack(config => {
-    // I'm giving into peer pressure.
-    ['css', 'scss', 'sass', 'less', 'stylus', 'postcss'].forEach(lang => {
-      config.module
-        .rule(lang)
-        .oneOf("normal")
-        .use("postcss-loader")
-        .tap(options => {
-          options.plugins.unshift(
-            ...[
-              shouldImport && postcssImport,
-              tailwind,
-              shouldTimeTravel && postcssPresetEnv
-            ]
-          );
+	api.chainWebpack(config => {
+		// I'm giving into peer pressure.
+		['css', 'scss', 'sass', 'less', 'stylus', 'postcss'].forEach(lang => {
+			config.module
+				.rule(lang)
+				.oneOf('normal')
+				.use('postcss-loader')
+				.tap(options => {
+					options.plugins.unshift(
+						...[
+							shouldImport && postcssImport,
+							tailwind,
+							shouldTimeTravel && postcssPresetEnv
+						]
+					);
 
-          process.env.NODE_ENV === "production" &&
+					// eslint-disable-next-line no-unused-expressions
+					process.env.NODE_ENV === 'production' &&
             shouldPurge &&
             options.plugins.push(purgecss);
 
-          return options;
-        });
-
-    })
-  });
+					return options;
+				});
+		});
+	});
 }
 
 TailwindPlugin.defaultOptions = () => ({
-  shouldPurge: true,
-  shouldImport: true,
-  shouldTimeTravel: true,
-  tailwindConfig: undefined,
-  presetEnvConfig: {
-    stage: 0,
-    autoprefixer: false
-  },
-  purgeConfig: {
-    content: [
-      "./src/**/*.vue",
-      "./src/**/*.js",
-      "./src/**/*.jsx",
-      "./src/**/*.ts",
-      "./src/**/*.tsx",
-      "./src/**/*.html",
-      "./src/**/*.pug",
-      "./src/**/*.md",
-      "./src/**/*.svg"
-    ],
-    whitelist: [
-      "body",
-      "html",
-      "img",
-      "a",
-      "g-image",
-      "g-image--lazy",
-      "g-image--loaded",
-      "active",
-      "active--exact"
-    ],
-    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-  }
+	shouldPurge: true,
+	shouldImport: true,
+	shouldTimeTravel: true,
+	tailwindConfig: undefined,
+	presetEnvConfig: {
+		stage: 0,
+		autoprefixer: false
+	},
+	purgeConfig: {
+		content: [
+			'./src/**/*.vue',
+			'./src/**/*.js',
+			'./src/**/*.jsx',
+			'./src/**/*.ts',
+			'./src/**/*.tsx',
+			'./src/**/*.html',
+			'./src/**/*.pug',
+			'./src/**/*.md',
+			'./src/**/*.svg'
+		],
+		whitelist: [
+			'body',
+			'html',
+			'img',
+			'a',
+			'g-image',
+			'g-image--lazy',
+			'g-image--loaded',
+			'active',
+			'active--exact'
+		],
+		defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+	}
 });
 
 module.exports = TailwindPlugin;
