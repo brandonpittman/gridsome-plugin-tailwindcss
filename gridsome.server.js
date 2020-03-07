8642d0baf468aa7c8c3df02df2d18b6fe8c0b375
Brandon Pittman <brandon@brandonpittman.net>
2020-03-07 17:44:14 +0900

    wip

--- gridsome.server.js
+++ gridsome.server.js
@@ -1,91 +1,91 @@
 function TailwindPlugin(api, options) {
-	const {
-		tailwindConfig,
-		purgeConfig,
-		presetEnvConfig,
-		shouldImport,
-		shouldPurge,
-		shouldTimeTravel
-	} = options;
+  const {
+    tailwindConfig,
+    purgeConfig,
+    presetEnvConfig,
+    shouldImport,
+    shouldPurge,
+    shouldTimeTravel
+  } = options;
 
-	const postcssImport = require('postcss-import')();
+  const postcssImport = require('postcss-import')();
 
-	const tailwind = tailwindConfig ?
-		require('tailwindcss')(tailwindConfig) :
-		require('tailwindcss');
+  const tailwind = tailwindConfig ?
+    require('tailwindcss')(tailwindConfig) :
+    require('tailwindcss');
 
-	const postcssPresetEnv = require('postcss-preset-env')(presetEnvConfig);
-	const purgecss = require('@fullhuman/postcss-purgecss')(purgeConfig);
+  const postcssPresetEnv = require('postcss-preset-env')(presetEnvConfig);
+  const purgecss = require('@fullhuman/postcss-purgecss')(purgeConfig);
 
-	api.chainWebpack(config => {
-		['css', 'scss', 'sass', 'less', 'stylus', 'postcss'].forEach(lang => {
-			config.module
-				.rule(lang)
-				.oneOf('normal')
-				.use('postcss-loader')
-				.tap(options => {
+  api.chainWebpack(config => {
+    ['css', 'scss', 'sass', 'less', 'stylus', 'postcss'].forEach(lang => {
+      config.module
+        .rule(lang)
+        .oneOf('normal')
+        .use('postcss-loader')
+        .tap(options => {
 
-					if (shouldImport) options.plugins.unshift(postcssImport)
+          if (shouldTimeTravel) options.plugins.unshift(postcssPresetEnv)
 
-					options.plugins.unshift(tailwind)
+          options.plugins.unshift(tailwind)
 
-					if (shouldTimeTravel) options.plugins.unshift(postcssPresetEnv)
+          if (shouldImport) options.plugins.unshift(postcssImport)
 
-					// eslint-disable-next-line no-unused-expressions
-					process.env.NODE_ENV === 'production' &&
-						shouldPurge &&
-						options.plugins.push(purgecss);
+          // eslint-disable-next-line no-unused-expressions
+          process.env.NODE_ENV === 'production' &&
+            shouldPurge &&
+            options.plugins.push(purgecss);
 
-					return options;
-				});
-		});
-	});
+          return options;
+        });
+    });
+  });
 }
 
 TailwindPlugin.defaultOptions = () => ({
-	shouldPurge: true,
-	shouldImport: true,
-	shouldTimeTravel: true,
-	tailwindConfig: undefined,
-	presetEnvConfig: {
-		stage: 0,
-		autoprefixer: false
-	},
-	purgeConfig: {
-		keyframes: true,
-		content: [
-			'./src/**/*.vue',
-			'./src/**/*.js',
-			'./src/**/*.jsx',
-			'./src/**/*.ts',
-			'./src/**/*.tsx',
-			'./src/**/*.html',
-			'./src/**/*.pug',
-			'./src/**/*.md',
-			'./src/**/*.svg'
-		],
-		whitelist: [
-			'body',
-			'html',
-			'img',
-			'a',
-			'g-image',
-			'g-image--lazy',
-			'g-image--loaded',
-			'active',
-			'active--exact'
-		],
-		whitelistPatterns: [
-			/shiki/,
-			/prism/,
-			/markdown/,
-			/.*-(enter|enter-active|enter-to|leave|leave-active|leave-to)/,
-			/data-v-.*/,
-			/>>>/,
-			/::v-deep/
-		],
-		defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || []
-	}
+  shouldPurge: true,
+  shouldImport: true,
+  shouldTimeTravel: true,
+  tailwindConfig: undefined,
+  presetEnvConfig: {
+    stage: 0,
+    autoprefixer: false
+  },
+  purgeConfig: {
+    keyframes: true,
+    content: [
+      './src/**/*.vue',
+      './src/**/*.js',
+      './src/**/*.jsx',
+      './src/**/*.ts',
+      './src/**/*.tsx',
+      './src/**/*.html',
+      './src/**/*.pug',
+      './src/**/*.md',
+      './src/**/*.svg'
+    ],
+    whitelist: [
+      'body',
+      'html',
+      'img',
+      'a',
+      'g-image',
+      'g-image--lazy',
+      'g-image--loaded',
+      'active',
+      'active--exact'
+    ],
+    whitelistPatterns: [
+      /shiki/,
+      /prism/,
+      /markdown/,
+      /.*-(enter|enter-active|enter-to|leave|leave-active|leave-to)/,
+      /data-v-.*/,
+      />>>/,
+      /::v-deep/
+    ],
+    defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || []
+  }
 });
 
 module.exports = TailwindPlugin;
